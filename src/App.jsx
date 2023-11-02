9be0680327fe49e3836a55ae842e9dc9516e9c17
Aya-jafar <aya.jafar002@gmail.com>
2023-11-02 19:45:32 +0300

    useReducer todo list example

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import UseReducerToDoList from "./components/ToDoList";
+
+function App() {
+  return (
+    <center>
+      <h1>To Do list using useReducer</h1>
+      <UseReducerToDoList />
+    </center>
+  );
+}
+
+export default App;
